958f1a3aabe1b7be6db81a15c65281ada0d36282
Raphael Scaziti <raphascaziti@gmail.com>
2024-10-05 22:17:38 -0600

    Layout finalized

--- Tic_Tac_Toe_group_7_sodv2101/Form1.Designer.cs
+++ Tic_Tac_Toe_group_7_sodv2101/Form1.Designer.cs
@@ -39,17 +39,21 @@
             button4 = new Button();
             button1 = new Button();
             panel2 = new Panel();
-            playerOScore = new Label();
             playerXScore = new Label();
-            label3 = new Label();
             label1 = new Label();
+            playerOScore = new Label();
+            label3 = new Label();
             panel3 = new Panel();
             exitBtn = new Button();
             resetBtn = new Button();
             newGameBtn = new Button();
+            panel4 = new Panel();
+            label2 = new Label();
+            label4 = new Label();
             panel1.SuspendLayout();
             panel2.SuspendLayout();
             panel3.SuspendLayout();
+            panel4.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
@@ -64,154 +68,168 @@
             panel1.Controls.Add(button7);
             panel1.Controls.Add(button4);
             panel1.Controls.Add(button1);
-            panel1.Location = new Point(25, 16);
+            panel1.Location = new Point(35, 70);
             panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(474, 552);
+            panel1.Size = new Size(384, 388);
             panel1.TabIndex = 0;
             // 
             // button9
             // 
-            button9.Location = new Point(328, 383);
+            button9.Font = new Font("Segoe UI", 30F);
+            button9.Location = new Point(255, 260);
             button9.Margin = new Padding(3, 4, 3, 4);
             button9.Name = "button9";
-            button9.Size = new Size(143, 167);
+            button9.Size = new Size(120, 120);
             button9.TabIndex = 0;
             button9.UseVisualStyleBackColor = true;
             button9.Click += button9_Click;
             // 
             // button6
             // 
-            button6.Location = new Point(328, 191);
+            button6.Font = new Font("Segoe UI", 30F);
+            button6.Location = new Point(255, 132);
             button6.Margin = new Padding(3, 4, 3, 4);
             button6.Name = "button6";
-            button6.Size = new Size(143, 167);
+            button6.Size = new Size(120, 120);
             button6.TabIndex = 0;
             button6.UseVisualStyleBackColor = true;
             button6.Click += button6_Click;
             // 
             // button3
             // 
-            button3.Location = new Point(328, 4);
+            button3.Font = new Font("Segoe UI", 30F);
+            button3.Location = new Point(255, 4);
             button3.Margin = new Padding(3, 4, 3, 4);
             button3.Name = "button3";
-            button3.Size = new Size(143, 167);
+            button3.Size = new Size(120, 120);
             button3.TabIndex = 0;
             button3.UseVisualStyleBackColor = true;
             button3.Click += button3_Click;
             // 
             // button8
             // 
-            button8.Location = new Point(167, 383);
+            button8.Font = new Font("Segoe UI", 30F);
+            button8.Location = new Point(129, 260);
             button8.Margin = new Padding(3, 4, 3, 4);
             button8.Name = "button8";
-            button8.Size = new Size(143, 167);
+            button8.Size = new Size(120, 120);
             button8.TabIndex = 0;
             button8.UseVisualStyleBackColor = true;
             button8.Click += button8_Click;
             // 
             // button5
             // 
-            button5.Location = new Point(167, 191);
+            button5.Font = new Font("Segoe UI", 30F);
+            button5.Location = new Point(129, 132);
             button5.Margin = new Padding(3, 4, 3, 4);
             button5.Name = "button5";
-            button5.Size = new Size(143, 167);
+            button5.Size = new Size(120, 120);
             button5.TabIndex = 0;
             button5.UseVisualStyleBackColor = true;
             button5.Click += button5_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(167, 4);
+            button2.Font = new Font("Segoe UI", 30F);
+            button2.Location = new Point(129, 4);
             button2.Margin = new Padding(3, 4, 3, 4);
             button2.Name = "button2";
-            button2.Size = new Size(143, 167);
+            button2.Size = new Size(120, 120);
             button2.TabIndex = 0;
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
             // 
             // button7
             // 
-            button7.Location = new Point(3, 383);
+            button7.Font = new Font("Segoe UI", 30F);
+            button7.Location = new Point(3, 260);
             button7.Margin = new Padding(3, 4, 3, 4);
             button7.Name = "button7";
-            button7.Size = new Size(143, 167);
+            button7.Size = new Size(120, 120);
             button7.TabIndex = 0;
             button7.UseVisualStyleBackColor = true;
             button7.Click += button7_Click;
             // 
             // button4
             // 
-            button4.Location = new Point(3, 191);
+            button4.Font = new Font("Segoe UI", 30F);
+            button4.Location = new Point(3, 132);
             button4.Margin = new Padding(3, 4, 3, 4);
             button4.Name = "button4";
-            button4.Size = new Size(143, 167);
+            button4.Size = new Size(120, 120);
             button4.TabIndex = 0;
             button4.UseVisualStyleBackColor = true;
             button4.Click += button4_Click;
             // 
             // button1
             // 
+            button1.Font = new Font("Segoe UI", 30F);
             button1.Location = new Point(3, 4);
             button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(143, 167);
+            button1.Size = new Size(120, 120);
             button1.TabIndex = 0;
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
             // panel2
             // 
+            panel2.BackColor = SystemColors.ControlDark;
             panel2.BorderStyle = BorderStyle.Fixed3D;
-            panel2.Controls.Add(playerOScore);
             panel2.Controls.Add(playerXScore);
-            panel2.Controls.Add(label3);
             panel2.Controls.Add(label1);
-            panel2.Location = new Point(542, 223);
+            panel2.Location = new Point(35, 471);
             panel2.Margin = new Padding(3, 4, 3, 4);
             panel2.Name = "panel2";
-            panel2.Size = new Size(358, 345);
+            panel2.Size = new Size(128, 50);
             panel2.TabIndex = 0;
-            // 
-            // playerOScore
-            // 
-            playerOScore.AutoSize = true;
-            playerOScore.Font = new Font("Segoe UI", 28F);
-            playerOScore.Location = new Point(233, 200);
-            playerOScore.Name = "playerOScore";
-            playerOScore.Size = new Size(52, 62);
-            playerOScore.TabIndex = 0;
-            playerOScore.Text = "0";
             // 
             // playerXScore
             // 
             playerXScore.AutoSize = true;
-            playerXScore.Font = new Font("Segoe UI", 28F);
-            playerXScore.Location = new Point(233, 61);
+            playerXScore.BackColor = SystemColors.ButtonHighlight;
+            playerXScore.BorderStyle = BorderStyle.Fixed3D;
+            playerXScore.Font = new Font("Segoe UI", 15F);
+            playerXScore.Location = new Point(91, 6);
             playerXScore.Name = "playerXScore";
-            playerXScore.Size = new Size(52, 62);
+            playerXScore.Size = new Size(30, 37);
             playerXScore.TabIndex = 0;
             playerXScore.Text = "0";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 25F);
-            label3.Location = new Point(32, 200);
-            label3.Name = "label3";
-            label3.Size = new Size(181, 57);
-            label3.TabIndex = 0;
-            label3.Text = "Player O";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 25F);
-            label1.Location = new Point(32, 61);
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(3, 10);
             label1.Name = "label1";
-            label1.Size = new Size(174, 57);
+            label1.Size = new Size(90, 28);
             label1.TabIndex = 0;
             label1.Text = "Player X";
+            // 
+            // playerOScore
+            // 
+            playerOScore.AutoSize = true;
+            playerOScore.BackColor = SystemColors.ButtonHighlight;
+            playerOScore.BorderStyle = BorderStyle.Fixed3D;
+            playerOScore.Font = new Font("Segoe UI", 15F);
+            playerOScore.Location = new Point(3, 5);
+            playerOScore.Name = "playerOScore";
+            playerOScore.Size = new Size(30, 37);
+            playerOScore.TabIndex = 0;
+            playerOScore.Text = "0";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label3.ForeColor = SystemColors.ButtonHighlight;
+            label3.Location = new Point(36, 10);
+            label3.Name = "label3";
+            label3.Size = new Size(92, 28);
+            label3.TabIndex = 0;
+            label3.Text = "Player O";
             // 
             // panel3
             // 
@@ -219,18 +237,18 @@
             panel3.Controls.Add(exitBtn);
             panel3.Controls.Add(resetBtn);
             panel3.Controls.Add(newGameBtn);
-            panel3.Location = new Point(542, 16);
+            panel3.Location = new Point(104, 542);
             panel3.Margin = new Padding(3, 4, 3, 4);
             panel3.Name = "panel3";
-            panel3.Size = new Size(358, 173);
+            panel3.Size = new Size(235, 64);
             panel3.TabIndex = 0;
             // 
             // exitBtn
             // 
-            exitBtn.Location = new Point(233, 51);
+            exitBtn.Location = new Point(155, 4);
             exitBtn.Margin = new Padding(3, 4, 3, 4);
             exitBtn.Name = "exitBtn";
-            exitBtn.Size = new Size(99, 72);
+            exitBtn.Size = new Size(70, 50);
             exitBtn.TabIndex = 0;
             exitBtn.Text = "Exit";
             exitBtn.UseVisualStyleBackColor = true;
@@ -238,10 +256,10 @@
             // 
             // resetBtn
             // 
-            resetBtn.Location = new Point(127, 51);
+            resetBtn.Location = new Point(79, 4);
             resetBtn.Margin = new Padding(3, 4, 3, 4);
             resetBtn.Name = "resetBtn";
-            resetBtn.Size = new Size(99, 72);
+            resetBtn.Size = new Size(70, 50);
             resetBtn.TabIndex = 0;
             resetBtn.Text = "Reset";
             resetBtn.UseVisualStyleBackColor = true;
@@ -249,20 +267,55 @@
             // 
             // newGameBtn
             // 
-            newGameBtn.Location = new Point(21, 51);
+            newGameBtn.Location = new Point(3, 4);
             newGameBtn.Margin = new Padding(3, 4, 3, 4);
             newGameBtn.Name = "newGameBtn";
-            newGameBtn.Size = new Size(99, 72);
+            newGameBtn.Size = new Size(70, 50);
             newGameBtn.TabIndex = 0;
-            newGameBtn.Text = "New Game";
+            newGameBtn.Text = "New";
             newGameBtn.UseVisualStyleBackColor = true;
             newGameBtn.Click += newGameBtn_Click;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = SystemColors.ControlDark;
+            panel4.BorderStyle = BorderStyle.Fixed3D;
+            panel4.Controls.Add(playerOScore);
+            panel4.Controls.Add(label3);
+            panel4.Location = new Point(291, 471);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(128, 50);
+            panel4.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 20F);
+            label2.Location = new Point(205, 475);
+            label2.Name = "label2";
+            label2.Size = new Size(40, 46);
+            label2.TabIndex = 2;
+            label2.Text = "X";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BorderStyle = BorderStyle.Fixed3D;
+            label4.Font = new Font("Segoe UI", 15F);
+            label4.Location = new Point(126, 19);
+            label4.Name = "label4";
+            label4.Size = new Size(207, 37);
+            label4.TabIndex = 3;
+            label4.Text = "Super Tic Tac Toe";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(914, 600);
+            ClientSize = new Size(439, 630);
+            Controls.Add(label4);
+            Controls.Add(label2);
+            Controls.Add(panel4);
             Controls.Add(panel3);
             Controls.Add(panel2);
             Controls.Add(panel1);
@@ -273,7 +326,10 @@
             panel2.ResumeLayout(false);
             panel2.PerformLayout();
             panel3.ResumeLayout(false);
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -297,5 +353,8 @@
         private Button exitBtn;
         private Button resetBtn;
         private Button newGameBtn;
+        private Panel panel4;
+        private Label label2;
+        private Label label4;
     }
 }
